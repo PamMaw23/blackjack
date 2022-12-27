93d6556bfc0f2f660592a970db9d5f1c510c39ea
Pamela Mawoyo <p_mawoyo@yahoo.com>
2022-12-27 18:59:15 -0500

    new_commit

---
+++ script2.js
@@ -0,0 +1,19 @@
+// let hasSolvedChallenge = true
+// let hasHintsLeft = false
+
+// if (hasSolvedChallenge===false && hasHintsLeft===false){
+//     showSolution() 
+// }
+// function showSolution(){
+// console.log("Showing the solution ....")
+// }
+
+let castle = {
+    title: "Live like a king",
+    price: 190,
+    isSuperHost: true,
+    images: ["img1", "img2", "img3"]
+}
+
+console.log(castle["price"])
+console.log(castle["title"])
